e62828143b12b01ebfba5a96152e976218aa1312
Niels <niels.minssen@etu.uca.fr>
2023-11-13 17:00:58 +0800

    lower size images

---
+++ python/optimise.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os
+
+def optimize_png_images(directory_path):
+    files = os.listdir(directory_path)
+    png_files = [f for f in files if f.lower().endswith('.png')]
+    for file in png_files:
+        file_path = os.path.join(directory_path, file)
+        image = Image.open(file_path)
+        image = image.convert("P", palette=Image.ADAPTIVE)
+        image.save(file_path, optimize=True, quality=85)  # Overwrite the original file
+    return f"Optimized {len(png_files)} PNG images."
+
+#Use the function like this:
+optimize_png_images('./data/images')
